f964579d6e72623117013a4580a8998a6b2a1ba6
elio.lima <diretoria@maxtime.info>
2019-05-07 11:00:03 -0300

    Revert "Ajuste"
    This reverts commit 05a4517aa662e272c88a757dce5442b5b6eb3354.

--- client/src/pages/acesso/pages/index.js
+++ client/src/pages/acesso/pages/index.js
@@ -60,29 +60,26 @@ class Objeto extends Component {
     <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as='h2' color='teal' textAlign='center'>
-            Acesso ao Sistema
+          <Image src='/logo.png' /> Log-in to your account
         </Header>
         <Form size='large'>
           <Segment stacked>
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='Endereço de E-mail' value={base64.decode(this.state.name)} onChange={e => this.setState({name: base64.encode(e.target.value)})} />
+            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
             <Form.Input
               fluid
               icon='lock'
               iconPosition='left'
               placeholder='Password'
               type='password'
-              value={base64.decode(this.state.pass)} onChange={e => this.setState({pass: base64.encode(e.target.value)})}
             />
 
-            <Button color='teal' 
-              fluid size='large'
-              onClick={(e) => this.onSubmit(e)} >
-              Entrar
+            <Button color='teal' fluid size='large'>
+              Login
             </Button>
           </Segment>
         </Form>
         <Message>
-           <a href='#'>Esqueci minha senha?</a>
+          New to us? <a href='#'>Sign Up</a>
         </Message>
       </Grid.Column>
     </Grid>
